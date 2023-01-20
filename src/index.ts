88ff757236ee48d79e90607c71f46d8a83a26111
Askar Yalyaev <askarrussia@gmail.com>
2023-01-21 01:01:31 +0300

    setup base server config

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
 import http from 'http';
 
-console.log('Hello world', http);
+const PORT = process.env.PORT || 8080;
+
+const server = http.createServer((request, response) => {
+  response.end('Server is working!');
+});
+
+server.listen(PORT, () => {
+  console.log(`Server has been started on port ${PORT} `);
+});
